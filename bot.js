084975d2aeca2bcd86c0fff53ea3c5189b52ab94
Aliq-M <a7749046@gmail.com>
2024-07-10 00:02:10 +0300

    dotenv + token+

--- bot.js
+++ bot.js
@@ -1,4 +1,7 @@
+require('dotenv').config();
 const { Bot, InlineKeyboard} = require('grammy'); 
 const express = require('express');
 const Knex = require('knex')(require('./knexfile').development);
 const logger = require('./logger');
+
+const bot = new Bot(process.env.TELEGRAM_BOT_TOKEN);
